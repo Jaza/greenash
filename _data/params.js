9bd37ce85c57fcd3f5e9b4239c1cb2cf95c08c23
Jeremy Epstein <jazepstein@gmail.com>
2021-12-25 22:02:29 +1100

    allow comments from author

--- _data/params.js
+++ _data/params.js
@@ -1,4 +1,6 @@
 module.exports = {
   lang: 'en-AU',
-  uploadsBaseURL: process.env.GREENASH_UPLOADS_BASE_URL
+  uploadsBaseURL: process.env.GREENASH_UPLOADS_BASE_URL,
+  authorName: 'Jeremy Epstein',
+  authorURL: 'https://greenash.net.au'
 };
