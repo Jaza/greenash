9242d96dc8e6d6e880525eb5c6d16f98a28bb514
Jeremy Epstein <jazepstein@gmail.com>
2021-12-29 11:38:40 +1100

    legal and footer

--- _data/params.js
+++ _data/params.js
@@ -2,5 +2,6 @@ module.exports = {
   lang: 'en-AU',
   uploadsBaseURL: process.env.GREENASH_UPLOADS_BASE_URL,
   authorName: 'Jaza',
-  authorURL: 'https://greenash.net.au'
+  authorURL: 'https://greenash.net.au',
+  footerText: `ABN: 58 377 192 424<br>&copy; 2004-${new Date().getFullYear()} Jeremy Epstein. <a href="/legal/">Legalities</a> apply.`
 };
