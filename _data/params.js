3cffe511f3c1fd441fffdcfe8bf7025f37a28048
Jeremy Epstein <jazepstein@gmail.com>
2022-01-05 23:07:04 +1100

    fix my birthday

--- _data/params.js
+++ _data/params.js
@@ -4,5 +4,5 @@ module.exports = {
   authorName: 'Jaza',
   authorURL: 'https://greenash.net.au',
   footerText: `ABN: 58 377 192 424<br>&copy; 2004-${new Date().getFullYear()} Jeremy Epstein. <a href="/legal/">Legalities</a> apply.`,
-  jazaDob: '1986-03-01'
+  jazaDob: '1986-01-03'
 };
