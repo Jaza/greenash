2b04e00d3d349116a3cde5f879046df5c735d156
Jeremy Epstein <jazepstein@gmail.com>
2022-03-13 23:18:35 +1100

    cloudflare analytics

--- _data/params.js
+++ _data/params.js
@@ -2,6 +2,7 @@ module.exports = {
   lang: 'en-AU',
   baseURL: process.env.GREENASH_BASE_URL,
   uploadsBaseURL: process.env.GREENASH_UPLOADS_BASE_URL,
+  cloudflareAnalyticsToken: process.env.CLOUDFLARE_ANALYTICS_TOKEN,
   authorName: 'Jaza',
   authorURL: 'https://greenash.net.au',
   footerText: `ABN: 58 377 192 424<br>&copy; 2004-${new Date().getFullYear()} Jeremy Epstein. <a href="/legal/">Legalities</a> apply.`,
