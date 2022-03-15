4b496ae16666686f338d5cad5a1f04932a71820c
Jeremy Epstein <jazepstein@gmail.com>
2022-03-15 18:24:47 +1100

    lil fixes

--- static/js/recaptchaloader.js
+++ static/js/recaptchaloader.js
@@ -9,15 +9,15 @@ ready(() => {
   submitButton.textContent = 'Prepare to ' + window.originalSubmitFormButtonText;
 
   submitButton.addEventListener("click", e => {
+    if (submitButton.textContent === window.originalSubmitFormButtonText) {
+      return;
+    }
+
     const agreeToCookiesMessage =
       'This will load Google reCAPTCHA, which will set cookies. Sadly, you will ' +
       'not be able to submit this form unless you agree. GDPR, not to mention ' +
       'basic human decency, dictates that you have a choice and a right to protect ' +
       'your privacy from the corporate overlords. Do you agree?';
-
-    if (submitButton.textContent === window.originalSubmitFormButtonText) {
-      return true;
-    }
 
     if (window.confirm(agreeToCookiesMessage)) {
       const recaptchaScript = document.createElement('script');
@@ -30,6 +30,5 @@ ready(() => {
     }
 
     e.preventDefault();
-    return false;
   });
 });
