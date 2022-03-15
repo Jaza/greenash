6ba8defbfa5c8d387acd5e37a16c06f470f27da2
Jeremy Epstein <jazepstein@gmail.com>
2022-03-15 17:02:27 +1100

    gdpr respectful recaptcha loading

--- _data/params.js
+++ _data/params.js
@@ -3,6 +3,7 @@ module.exports = {
   baseURL: process.env.GREENASH_BASE_URL,
   uploadsBaseURL: process.env.GREENASH_UPLOADS_BASE_URL,
   cloudflareAnalyticsToken: process.env.CLOUDFLARE_ANALYTICS_TOKEN,
+  recaptchaKey: process.env.SITE_RECAPTCHA_KEY,
   authorName: 'Jaza',
   authorURL: 'https://greenash.net.au',
   footerText: `ABN: 58 377 192 424<br>&copy; 2004-${new Date().getFullYear()} Jeremy Epstein. <a href="/legal/">Legalities</a> apply.`,
--- static/js/ready.js
+++
@@ -1,5 +0,0 @@
-// Thanks to: https://tobiasahlin.com/blog/move-from-jquery-to-vanilla-javascript/
-const ready = (callback) => {
-  if (document.readyState != "loading") callback();
-  else document.addEventListener("DOMContentLoaded", callback);
-}
